77762ad7d7f40e46bfb621737c9f2ad4309b741a
NeIIor <egor.gabisov@gmail.com>
2024-08-16 20:55:31 +0300

    Create quadratic_equation.c

---
+++ quadratic_equation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a = NAN, b = NAN, c = NAN, d = NAN;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    d = pow(b, 2) - 4 * a * c;
+    if (d < 0) {
+        printf("No roots\n");
+    }
+    else if (d < 0.000000000000001) {
+        printf("One root: %lf\n", -b/(2 * a));
+    }
+    else {
+        printf("Two roots: %lf %lf\n", (- b - sqrt(d))/(2 * a), (- b + sqrt(d))/(2 * a));
+    }
+    return 0;
+}
